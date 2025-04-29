18a9229cec43189d7617ac430504ae4f4815ffd7
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-04-29 16:13:59 -0300

    Oientação a objetos - classes, objetos atrbutos e métodos. Classe Carro: Criando e instanciando primeira classe

--- angular/typescript/app.ts
+++ angular/typescript/app.ts
@@ -4,6 +4,8 @@ let mensagem: string = "Seja bem vindo"
 let temporadaFriends: number = 10
 let estudandoAngula: boolean = true
 let listaDeFrutas: Array<string> = ['uva', 'banana', 'laranja']
+let listaDeFrutas2: string[] = ['abacate', 'morango', 'coco']
+
 let notasDasProvas: Array<number> = [9,7,8,9]
 
 mensagem = 'Eba'
--- angular/typescript/dist/app.js
+++ angular/typescript/dist/app.js
@@ -3,5 +3,6 @@ let mensagem = "Seja bem vindo";
 let temporadaFriends = 10;
 let estudandoAngula = true;
 let listaDeFrutas = ['uva', 'banana', 'laranja'];
+let listaDeFrutas2 = ['abacate', 'morango', 'coco'];
 let notasDasProvas = [9, 7, 8, 9];
 mensagem = 'Eba';
