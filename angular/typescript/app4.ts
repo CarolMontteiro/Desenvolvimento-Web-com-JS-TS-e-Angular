5918784fe68e6c1ae4bae38577c33fe3ba01588c
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-05-14 17:32:45 -0300

    foi criada três classes DAO fazendo com que essas classes responsáveis por perdidtirem os objetos, implementem uma interface que vai dizer o que cada uma delas precisa obrigatoriamente implementar. Aideia é que tenhamos uma noção mais clara de como nós podeos utilizar o recurso de interfaces em em determinadas situações a aplicação

---
+++ angular/typescript/app4.ts
@@ -0,0 +1,25 @@
+import { ConcessionariaDao } from './ConcessionariaDao'
+import Concessionaria from './Concessionaria'
+
+import { PessoaDao } from './PessoaDao'
+import Pessoa from './Pessoa'
+
+import { CarroDao } from './CarroDao'
+import Carro from './Carro'
+
+
+
+let dao: ConcessionariaDao = new ConcessionariaDao()
+let concessionaria = new Concessionaria('', [])
+
+dao.inserir(concessionaria)
+
+let dao2: PessoaDao = new PessoaDao()
+let pessoa: Pessoa = new Pessoa('','')
+
+dao2.atualizar(pessoa)
+
+let dao3: CarroDao = new CarroDao()
+let carro: Carro = new Carro('',1)
+
+dao3.remover(0)
